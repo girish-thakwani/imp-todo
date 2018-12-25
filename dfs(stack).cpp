fb0d5dc7b2db7ec14bea45698fed27dab237c1e6
Girish Thakwani <30826734+girish-thakwani@users.noreply.github.com>
2018-12-25 16:04:11 +0530

    Create dfs(stack).cpp

---
+++ dfs(stack).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<stack>
+#include<iostream>
+using namespace std;
+#define N 100
+vector <int> v[N];
+void dfs(int source,int n){
+        int visited[n]={0};
+        visited[source]=1;
+        stack <int> s;
+        s.push(source);
+        while(!s.empty()){
+                int p=s.top();
+                s.pop();
+                cout<<p;
+                for(int i=0;i<v[p].size();i++){
+                        if(!visited[v[p][i]]){
+                                visited[v[p][i]]=1;
+                                s.push(v[p][i]);
+                        }
+                }
+        }
+}
+int main(){
+        int n,edges;cin>>n>>edges;
+        for(int i=0;i<edges;i++){
+                int x,y;cin>>x>>y;
+                v[x].push_back(y);
+                v[y].push_back(x);
+        }
+        int source;cin>>source;
+        dfs(source,n);
+}
